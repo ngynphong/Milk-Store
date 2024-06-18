80de7f05e0527ef32ee58f74e2ac326a8b212a36
daoduonghunganh <anhddhse170283@fpt.edu>
2024-06-18 21:35:54 +0700

    Anh

---
+++ client/myapp/src/pages/return/index.jsx
@@ -0,0 +1,47 @@
+
+import './index.scss';
+
+const ReturnPolicy = () => {
+  return (
+    <div className="return-policy">
+      <h1>CHÍNH SÁCH ĐỔI TRẢ</h1>
+      
+      <section>
+        <h2>QUY ĐỊNH ĐỔI TRẢ, THU HỒI SẢN PHẨM</h2>
+        <h3>I. ĐỔI TRẢ SẢN PHẨM</h3>
+        <h4>1. Điều kiện đổi trả sản phẩm:</h4>
+        <ul>
+          <li>Sản phẩm không đảm bảo chất lượng do lỗi của NSX hoặc do lỗi vận chuyển.</li>
+          <li>Sản phẩm không đúng với đơn đặt hàng của Khách hàng.</li>
+          <li>Sản phẩm đổi phải còn mới, nguyên hộp (không lệ hộp), chưa qua sử dụng.</li>
+          <li>Hoặc các sản phẩm có cam kết đổi trả nếu Khách hàng không sử dụng hết.</li>
+        </ul>
+        <p><strong>Lưu ý:</strong></p>
+        <ul>
+          <li>Trường hợp sản phẩm được bán theo chương trình khuyến mãi, không được trả hàng theo quy định của chương trình khuyến mãi.</li>
+          <li>Khách hàng chụp lại hình sản phẩm và gửi về email cskh@medsi.vn với tiêu đề “TRẢ HÀNG”, chúng tôi sẽ liên hệ và thu hồi lại sản phẩm lỗi.</li>
+        </ul>
+
+        <h4>2. Thời gian đổi trả</h4>
+        <ul>
+          <li>Nhận lại sản phẩm trong vòng 3 ngày kể từ ngày nhận được thông tin.</li>
+          <li>Trả lại hàng chậm nhất là 7 ngày làm việc kể từ ngày nhận được sản phẩm thu hồi.</li>
+        </ul>
+
+        <h3>II. THU HỒI SẢN PHẨM</h3>
+        <ul>
+          <li>Sản phẩm thuộc diện phải thu hồi theo yêu cầu của các Cơ quan chức năng hoặc của NSX.</li>
+          <li>Các sản phẩm có cam kết thu hồi nếu Khách hàng không sử dụng hết.</li>
+        </ul>
+
+        <h3>III. CHI PHÍ ĐỔI TRẢ VÀ THU HỒI</h3>
+        <ul>
+          <li>Công ty sẽ chịu 100% phí vận chuyển cho Khách hàng nếu sản phẩm đổi đáp ứng các điều kiện đã nêu ở Mục 1 (trường hợp đổi trả).</li>
+          <li>Công ty trả lại tiền cho Khách hàng với giá trị tương đương với số lượng thu hồi (trường hợp thu hồi).</li>
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default ReturnPolicy;
